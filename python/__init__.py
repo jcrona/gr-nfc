769bfd816f20267a89bb2a0c1aeabd2099ca308c
cjq <812206236@qq.com>
2020-12-04 23:49:51 +0800

    upgrade OOT to 3.8
    Signed-off-by: cjq <812206236@qq.com>

--- python/__init__.py
+++ python/__init__.py
@@ -22,13 +22,14 @@
 This is the GNU Radio NFC module. Place your Python package
 description here (python/__init__.py).
 '''
+from __future__ import unicode_literals
 
 # import swig generated symbols into the nfc namespace
 try:
-	# this might fail if the module is python-only
-	from nfc_swig import *
+    # this might fail if the module is python-only
+    from .nfc_swig import *
 except ImportError:
-	pass
+    pass
 
 # import any pure python here
 #
